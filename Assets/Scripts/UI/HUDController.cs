444466258891b5847c7f4baf035bfd1c8a7af6c8
Hilal Fitri <hilal.f@nus.edu.sg>
2022-08-21 15:27:25 +0800

    Level up system + item done functionally done

--- Assets/Scripts/UI/HUDController.cs
+++ Assets/Scripts/UI/HUDController.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour
 {
-    int[] itemSlotEffectIndex = new int[10];
+    public TMP_Text levelText;
+    public Image expFillBar;
+
+    [Header("Item Slots")]
+    public int[] itemSlotEffectIndex = new int[10];
+    public GameObject[] itemGameObj;
+    public Image[] itemImages;
+    public TMP_Text[] itemLevels;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +25,45 @@ public class HUDController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateXPBar(float valPercent)
+    {
+        expFillBar.fillAmount = valPercent;
+    }
+
+    public void UpdateLevel(int currLevel)
+    {
+        levelText.text = "LV " + currLevel;
+    }
+
+    public void UpdateItemSlot(int itemEffectIndex, ref ItemEffect ieRef)
+    {
+        if (itemEffectIndex >= 6) return;
+
+        int i = 0;
+        for (; i < itemSlotEffectIndex.Length; ++i)
+        {
+            if(itemSlotEffectIndex[i] == itemEffectIndex)
+            {
+                itemLevels[i].text = "" + ieRef.currLevel;
+
+                return;
+            }
+
+            if(itemSlotEffectIndex[i] == -1)
+            {
+                break;
+            }
+        }
+
+        if(i < itemSlotEffectIndex.Length)
+        {
+            itemSlotEffectIndex[i] = itemEffectIndex;
+            itemGameObj[i].SetActive(true);
+            itemImages[i].sprite = ieRef.itemPic;
+            itemLevels[i].text = "" + ieRef.currLevel;
+        }
+
     }
 }
