1738ebeeb6b3e740b9608f1c630cc104d6d9316c
Rozen <bomberdude@hotmail.com>
2022-08-21 16:26:23 +0800

    Lavae

---
+++ Assets/Scripts/Projectiles/LaeveProjectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaeveProjectile : BaseProjectile
+{
+    SpriteRenderer playerSprite;
+    public TrailRenderer trail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSprite = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().spriteRenderer;
+
+        if (playerSprite.flipX == false)
+        {
+            speed *= -1f;
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Duration();
+        Movement();
+        if (spriteRenderer.color.a < 1f) trail.emitting = false;
+
+    }
+    override protected void Movement()
+    {
+        rigidbody2d.velocity = direction * speed * Time.deltaTime;
+    }
+}
